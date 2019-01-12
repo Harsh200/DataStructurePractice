dce61f11f67971624b78c3f9581a1d6d92190143
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-01-12 14:02:14 +0530

    this is a stack implementation program through java. this is main program

--- src/stackimplementation.java
+++ src/stackimplementation.java
@@ -1,2 +1,31 @@
+import java.util.*;
 public class stackimplementation {
-}
+        public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter no to store in stack");
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            int d=sc.nextInt();
+            int e=sc.nextInt();
+            int f=sc.nextInt();
+            Intstack obj=new Intstack();
+            if(!obj.isfull()){
+                obj.push(a);
+                obj.push(b);
+                obj.push(c);
+                obj.push(d);
+                obj.push(e);
+                obj.push(f);
+            }
+            System.out.println("pop element is " + "=" +obj.pop());
+            System.out.println("pop element is " + "=" +obj.pop());
+            System.out.println("pop element is " + "=" +obj.pop());
+            System.out.println("pop element is " + "=" +obj.pop());
+            System.out.println("pop element is " + "=" +obj.pop());
+            System.out.println("pop element is " + "=" +obj.pop());
+        }
+    }
+
+
+
